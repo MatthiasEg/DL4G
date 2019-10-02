05ba494db89e1bccac7555f9977062d47a75b305
Matthias Egli <matthias.egli@stud.hslu.ch>
2019-10-02 22:49:28 +0200

    first implementation of trump decision according to amount of cards of a color and their values

--- my_jass/player/my_player.py
+++ my_jass/player/my_player.py
@@ -5,7 +5,7 @@
 
 import numpy as np
 
-from jass.base.const import color_masks
+from jass.base.const import color_masks, card_values
 from jass.base.player_round import PlayerRound
 from jass.player.player import Player
 
@@ -14,6 +14,7 @@ class MyPlayer(Player):
     """
     Sample implementation of a player to play Jass.
     """
+
     def select_trump(self, rnd: PlayerRound) -> int:
         """
         Player chooses a trump based on the given round information.
@@ -25,17 +26,60 @@ class MyPlayer(Player):
             selected trump
         """
         # select the trump with the largest number of cards
-        print(rnd.hand)
-        print(rnd.hand.shape)
+        # if there are two color with the same highest number of cards, it will calculate the values of each color
+        # and define trump according to the highest color cards.
+        print("Hand", rnd.hand)
 
         trump = 0
         max_number_in_color = 0
+        second_color_with_highest_number = 0
+        third_color_with_highest_number = 0
         for c in range(4):
             number_in_color = (rnd.hand * color_masks[c]).sum()
             if number_in_color > max_number_in_color:
                 max_number_in_color = number_in_color
                 trump = c
+                second_color_with_highest_number = 0
+                third_color_with_highest_number = 0
+            elif number_in_color == max_number_in_color:
+                if second_color_with_highest_number != 0:
+                    third_color_with_highest_number = c
+                else:
+                    second_color_with_highest_number = c
+
+        # action if hand has multiple colors with same highest amount of cards
+        if second_color_with_highest_number != 0:
+            total_value_of_trump_color_cards = self.calculateColorCardValues(trump, rnd)
+            total_value_of_second_color_cards = self.calculateColorCardValues(second_color_with_highest_number, rnd)
+
+            print("trump color: ", trump)
+            print("value of trump color", total_value_of_trump_color_cards)
+            print("second color: ", second_color_with_highest_number)
+            print("value of second color", total_value_of_second_color_cards)
+
+            if third_color_with_highest_number != 0:
+                total_value_of_third_color_cards = self.calculateColorCardValues(third_color_with_highest_number,
+                                                                                 rnd)
+                print("third color: ", third_color_with_highest_number)
+                print("value of third color", total_value_of_third_color_cards)
+
+                if total_value_of_second_color_cards > total_value_of_trump_color_cards and \
+                        total_value_of_second_color_cards > total_value_of_third_color_cards:
+                    trump = second_color_with_highest_number
+                elif total_value_of_third_color_cards > total_value_of_trump_color_cards:
+                    trump = third_color_with_highest_number
+
+            if total_value_of_second_color_cards > total_value_of_trump_color_cards and \
+                    third_color_with_highest_number == 0:
+                trump = second_color_with_highest_number
+
+        print("defining trump as: ", trump)
         return trump
+
+    def calculateColorCardValues(self, possibletrumpcolor: int, rnd: PlayerRound) -> int:
+        only_cards_of_this_color = rnd.hand * color_masks[possibletrumpcolor]
+        totol_value_of_given_color = (only_cards_of_this_color * card_values[possibletrumpcolor]).sum()
+        return totol_value_of_given_color
 
     def play_card(self, rnd: PlayerRound) -> int:
         """
